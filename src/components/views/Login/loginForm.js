7a6516663d2d52b54e0108c97ef733d30699633d
Doris <DorisMac@MikeMacBookPro.local>
2018-07-17 14:04:01 +0200

    Added login buttons

--- src/components/views/Login/loginForm.js
+++ src/components/views/Login/loginForm.js
@@ -35,6 +35,19 @@ class LoginForm extends Component {
     }
   }
 
+  updateInput = (name, value) => {
+    this.setState({
+      hasErrors:false
+    })
+
+    let formCopy = this.state.form
+    formCopy[name].value = value
+
+    this.setState({
+      form: formCopy
+    })
+  }
+
   render(){
     return(
       <View>
@@ -42,7 +55,7 @@ class LoginForm extends Component {
           placeholder="Enter your Email"
           type={this.state.form.email.type}
           value={this.state.form.email.value}
-          onChangeText={ value => () => alert('hey')}
+          onChangeText={ value => () => this.updateInput('email', value)}
           autoCapitalize={"none"}
           keyboardType={"email-address"}
         />
@@ -52,9 +65,25 @@ class LoginForm extends Component {
           type={this.state.form.password.type}
           value={this.state.form.password.value}
           onChangeText={ value => () => alert('password!')}
-          autoCapitalize={"none"}
-          keyboardType={"email-address"}
+          secureTextEntry
+
         />
+
+        <View>
+          <Button
+            title='login'
+            color='#fd9727'
+            onPress={() => alert('action')}
+          />
+        </View>
+
+        <View>
+          <Button
+            title='Ill do it later'
+            color='lightgrey'
+            onPress={() => alert('do it later')}
+          />
+        </View>
       <Text>Hello</Text>
     </View>
     )
